f3c2f6ac24ed16fce3466411294ad91a8f9dfb43
Leonidushka <47792923+Leonidushka@users.noreply.github.com>
2020-12-02 21:59:18 +0300

    добавлен комментарий

--- SimpleCodeCalc 02.12/SimpleCodeCalc 02.12/Program.cs
+++ SimpleCodeCalc 02.12/SimpleCodeCalc 02.12/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿// калькулятор будет доработан на WinForms
+
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
